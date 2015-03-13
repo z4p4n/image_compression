6069464636ca176f870846e5ab43cb72ec8fca36
root <root@vps116665.ovh.net>
2015-03-13 13:29:49 +0100

    D2.py

--- D2.py
+++ D2.py
@@ -5,76 +5,39 @@
 # z4p4n, NexMat
 
 import numpy as np
-
-d2_1 = np.array([
-	[1/2, 0, 0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0 ],
-	[1/2, 0, 0, 0, 0, 0, 0, 0, -1/2, 0, 0, 0, 0, 0, 0, 0],
-	[0, 1/2, 0, 0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0 ],
-	[0, 1/2, 0, 0, 0, 0, 0, 0, 0, -1/2, 0, 0, 0, 0, 0, 0],
-	[0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0 ],
-	[0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, -1/2, 0, 0, 0, 0, 0],
-	[0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0 ],
-	[0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, -1/2, 0, 0, 0, 0],
-	[0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0 ],
-	[0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, -1/2, 0, 0, 0],
-	[0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, 1/2, 0, 0 ],
-	[0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, -1/2, 0, 0],
-	[0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, 1/2, 0 ],
-	[0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, -1/2, 0],
-	[0, 0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, 1/2 ],
-	[0, 0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0, -1/2]], float);
-
-vect = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11, 30, 30,  100]);
-
-d2_2 = np.array([
-    [1/2, 0, 0, 0, 1/2, 0, 0, 0 ],
-    [1/2, 0, 0, 0, -1/2, 0, 0, 0],
-    [0, 1/2, 0, 0, 0, 1/2, 0, 0 ],
-    [0, 1/2, 0, 0, 0, -1/2, 0, 0],
-    [0, 0, 1/2, 0, 0, 0, 1/2, 0 ],
-    [0, 0, 1/2, 0, 0, 0, -1/2, 0],
-    [0, 0, 0, 1/2, 0, 0, 0, 1/2 ],
-    [0, 0, 0, 1/2, 0, 0, 0, -1/2]], float);
-
-d2_3 = np.array([
-    [1/2, 0, 1/2, 0 ],
-    [1/2, 0, -1/2, 0],
-    [0, 1/2, 0, 1/2 ],
-    [0, 1/2, 0, -1/2]], float);
-
-d2_4 = np.array([
-    [1/2, 1/2 ],
-    [1/2, -1/2]], float);
+	
 
 d2_1_inv = np.linalg.inv(d2_1);
 d2_2_inv = np.linalg.inv(d2_2);
 d2_3_inv = np.linalg.inv(d2_3);
 d2_4_inv = np.linalg.inv(d2_4);
 
+def matrixD2 (size):
+# Creation de la matrice des ondelettes de Haar
+
+	matrix = []
+	for i in range (size):
+		line = []
+		for j in range (size):
+			if j < size/2 and i//2 == j:
+				line.append(1/2)
+			elif j >= size/2 and (i%2 == 0) and (i//2 == j-size/2) :
+				line.append(1/2)
+			elif j >= size/2 and (i%2 == 1) and (i//2 == j-size/2) :
+				line.append(-1/2)
+			else :
+				line.append(0)
+
+		matrix.append (line)
+
+	return np.array (matrix, float)
 
 def transformationD2(vect):
-    print("vect : ", vect);
-    vect = np.dot(vect, d2_1);
-    print("vect2: ", vect);
-    S1 = vect[0:8];
-    D1 = vect[8:];
+    vect = np.dot(vect, matrixD2(len (vect)));
+    S = vect[0:len (vect)//2];
+    D = vect[len(vect)//2:];
 
-    vect = np.dot(S1, d2_2);
-    print("vect3: ", vect);
-    S2 = vect[0:4];
-    D2 = vect[4:];
-
-    vect = np.dot(S2, d2_3);
-    print("vect4: ", vect);
-    S3 = vect[0:2];
-    D3 = vect[2:];
-
-    vect = np.dot(S3, d2_4);
-    print("vect5: ", vect);
-    S4 = vect[0:1];
-    D4 = vect[1:];
-
-    return (S4, D1, D2, D3, D4)
+    return (S, D)
 
 def transformation_inverse(S4, D1, D2, D3, D4):
     SD4 = np.concatenate((S4, D4), axis = 0);
@@ -109,3 +72,10 @@ def YUV_to_RGB():
             [ 1, -0.39465, -0.58060],
             [ 1,  2.03211,  0      ]);
             
+if __name__ == '__main__':
+
+	print (matrixD2 (2))
+	print (matrixD2 (4))
+	print (matrixD2 (6))
+	print (matrixD2 (8))
+
